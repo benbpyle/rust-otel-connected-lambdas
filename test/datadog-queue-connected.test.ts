54ee203b2ea1973a1008e0f91228e862a96314bf
Benjamen Pyle <benbpyle@gmail.com>
2025-02-17 16:34:57 -0600

    Initial commit

---
+++ test/datadog-queue-connected.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DatadogQueueConnected from '../lib/datadog-queue-connected-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/datadog-queue-connected-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DatadogQueueConnected.DatadogQueueConnectedStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
